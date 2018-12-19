27deb32c15d86da793c50779f46c6dce18fe40ae
Sean Dooher <sean@dooher.net>
2018-12-19 12:46:59 -0800

    Add react hot reloading

--- ohqueue/webpack.config.js
+++ ohqueue/webpack.config.js
@@ -1,32 +1,35 @@
-var path = require('path');
+var path = require("path");
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
+
 module.exports = {
   context: __dirname,
-
-  entry: ['./frontend/assets/js/index'],
-
+  entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './frontend/assets/js/index'
+  ],
   output: {
       path: path.resolve('./frontend/assets/bundles/'),
-      filename: '[name].js',
-  },
-
-  module: {
-    loaders: [
-      { 
-      	test: /\.jsx?$/,
-      	exclude: /node_modules/,
-      	loader: 'babel-loader',
-     	}, // to transform JSX into JS
-    ],
+      filename: "[name].js",
+      publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
   },
 
   plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
+    new BundleTracker({filename: './webpack-stats.json'}),
   ],
+
+  module: {
+    loaders: [
+      // we pass the output from babel loader to react-hot loader
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel-loader'], },
+    ],
+  },
 
   resolve: {
     extensions: ['.js', '.jsx']
-  }
-};
+  },
+}
