c302ebad303ddfc56130be83de2855a561e1983e
Sean Dooher <sean@dooher.net>
2019-01-03 02:38:43 -0800

    Add emission tests

--- ohqueue/backend/tests/async_tests.py
+++ ohqueue/backend/tests/async_tests.py
@@ -8,6 +8,7 @@ from django.test import Client
 
 from .sync_tests import TestUtils
 from backend.models import Ticket, TicketStatus, TicketEvent, TicketEventType
+from backend.models import ticket_status_to_event
 
 logging.disable(logging.ERROR)
 
@@ -78,4 +79,69 @@ class TestLeaves(ConsumerTests):
         ticket.status = TicketStatus.assigned.value
         ticket.save()
 
-        assert await client.receive_nothing(), await client.receive_json_from()
+        assert await client.receive_nothing()
+
+    async def test_event_create(self, profile, disconnect):
+        client = await self.create_client(profile.user, disconnect)
+        await self.subscribe(client, 'events', 'create')
+
+        ticket = Ticket.objects.create(
+            status=TicketStatus.pending.value,
+            student=profile,
+            assignment="Project 3",
+            question="Question 1",
+            location="Morgan 255",
+            description="[Conceptual] Need help with make client"
+        )
+
+        event = TicketEvent.objects.create(
+            event_type=TicketEventType.create.value,
+            ticket=ticket, user=profile
+        )
+
+        response = await client.receive_json_from()
+
+        assert response['stream'] == 'events'
+        assert response['payload']['action'] == 'create'
+
+    @pytest.mark.parametrize("status", TicketStatus)
+    async def test_event_emission(self, status, profile, staff_client, tickets, disconnect):
+        client = await self.create_client(profile.user, disconnect)
+        await self.subscribe(client, 'events', 'all')
+
+        result = staff_client.put('/api/staffticket/next', {'status': status.value})
+        ticket = Ticket.objects.get(id=result.json()['id'])
+
+        event_types = [status.value for status in ticket_status_to_event(TicketStatus.pending, status)]
+
+        for _ in event_types:
+            response = await client.receive_json_from()
+            ticket_event = response['payload']['data']
+
+            assert ticket_event['ticket'] == ticket.id
+            assert ticket_event['event_type'] in event_types
+
+        assert await client.receive_nothing()
+
+    async def test_event_emission_resolve(self, profile, staff_client, tickets, disconnect):
+        client = await self.create_client(profile.user, disconnect)
+        await self.subscribe(client, 'events', 'all')
+
+        result = staff_client.put('/api/staffticket/next', {'status': TicketStatus.assigned.value})
+        ticket = Ticket.objects.get(id=result.json()['id'])
+
+        response = await client.receive_json_from()
+        ticket_event = response['payload']['data']
+
+        assert ticket_event['ticket'] == ticket.id
+        assert ticket_event['event_type'] == TicketEventType.assign.value
+
+        result = staff_client.put(f'/api/staffticket/{ticket.id}', {'status': TicketStatus.resolved.value})
+
+        response = await client.receive_json_from()
+        ticket_event = response['payload']['data']
+
+        assert ticket_event['ticket'] == ticket.id
+        assert ticket_event['event_type'] == TicketEventType.resolve.value
+
+        assert await client.receive_nothing()
